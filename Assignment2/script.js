ba477909d400b9e56ac861b54bf28dfa3307cb7c
Md. Ridowan Ahmed <ridowan.ahmed763@gmail.com>
2025-11-16 22:39:53 +0600

    Update script.js
    Some how the previous file don't contain all the code. May be push problem. Now the full code is given.

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,83 +1,150 @@
-function search() {
-    const name = document.getElementById("take-input").value;
-
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            showItems(data.meals);
-        })
-        .catch(err => {
-            console.error("Error: ", err);
-            showNoItem();
-        })
+function init() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        itemList(data["drinks"]);
+    })
+    .catch(err => console.log("Error: ", err));
 }
 
-function showItems(meals) {
-    const show_item = document.getElementById("show-item");
-    show_item.innerHTML = "";
+document.addEventListener('DOMContentLoaded', init);
 
-    for (const meal of meals) {
-        const img = meal.strMealThumb;
-        const title = meal.strMeal;
+function createCard(item) {
+    const name = item["strDrink"];
+    const img = item["strDrinkThumb"];
+    const category = item["strCategory"];
+    const instruciton = item["strInstructions"];
 
-        const card = document.createElement("div");
-        card.innerHTML = `
-            <div class="card" style="width: 18rem;">
-                <img src="${img}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${title}</h5>
-                </div>
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="card my-3" style="width: 16rem;">
+            <img src="${img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p> Category: ${category}</p>
+                <p class="card-text">Insturction: ${instruciton.slice(0, 15)}</p>
+                <button type="button" class="btn btn-outline-danger abtn">Add To Cart</button>
+                <button type="button" class="btn btn-outline-info dbtn">Details</button>
             </div>
-        `
+        </div>
+    `
+    const dbtn = div.querySelector(".dbtn")
+    dbtn.addEventListener("click", () => showDetails(item["idDrink"]));
+    
+    const abtn = div.querySelector(".abtn");
+    abtn.addEventListener("click", () => addCart(item["strDrink"], item["strDrinkThumb"]));
 
-        card.addEventListener("click", () => {
-            const show_div = document.getElementById("details-show");
-            show_div.innerHTML = "";
+    return div;
+}
 
-            const img = meal.strMealThumb;
-            const title = meal.strMeal;
+function itemList(items) {
+    const item_list = document.getElementById("item-list");
+    item_list.innerHTML = "";
 
-            const card = document.createElement("div");
-            card.innerHTML = `
-                <div class="card" style="width: 18rem;">
-                    <img src="${img}" class="card-img-top" alt="...">
-                    <div class="card-body" id="detail-card">
-                        <h5 class="card-title">${title}</h5>
-                    </div>
-                </div>
-            `
-            show_div.append(card);
-
-            const detail_card = document.getElementById("detail-card");
-            const ingre = Object.keys(meal).filter(str => {
-                return str.includes("strIngredient");
-            })
-
-            const ul = document.createElement("ul");
-            for (let i of ingre) {
-                if (meal[i] == "") {
-                    continue;
-                }
-                
-                const li = document.createElement("li");
-                li.textContent = meal[i];
-                ul.appendChild(li);
-            }
-
-            detail_card.appendChild(ul);
-        })
-
-        show_item.appendChild(card);
+    for (let item of items) {
+        const card = createCard(item);
+        item_list.appendChild(card);
     }
 }
 
-function showNoItem() {
-    const show_item = document.getElementById("show-item");
-    show_item.innerHTML = "";
+function showDetails(id) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data);
+        createModal(data["drinks"][0]);
+    })
+    .catch(err => {
+        console.log("Error: ", err);
+    })
+}
 
-    show_item.innerHTML = `
-        <center><font size=20>No Items Found</font></center>
+function createModal(item) {
+    console.log(item);
+    const name = item["strDrink"];
+    const img = item["strDrinkThumb"];
+    const category = item["strCategory"];
+    const alcoholic = item["strAlcoholic"];
+    const instruction = item["strInstructions"];
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">${name}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img src="${img}" class="card-img-top" alt="...">
+                <p><b>Details</b></p>
+                <p>Category: <b>${category}</b></p>
+                <p>Alcoholic: <b>${alcoholic}</b></p>
+                <p>${instruction}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    `
+
+    document.body.appendChild(div.firstElementChild);
+    const modalElement = document.body.lastElementChild;
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+}
+
+
+let globalCount = 1;
+function addCart(name, img) {
+    if (globalCount == 8) {
+        alert("Don't drink to much");
+        return;
+    }
+
+    const cart = document.getElementById("cart");
+    const div = document.createElement("div")
+    div.innerHTML = `
+        <div class="row">
+            <div class="col-2">${globalCount}</div>
+            <div class="col-4">
+                <img src="${img}" class="card-img-top" alt="..." style="border-radius: 50%;">
+            </div>
+            <div class="col-6">${name}</div>
+        </div>
+        <hr>
+    `
+
+    const tcount = document.getElementById("Tcount");
+    tcount.innerText = "Total Cart: " + globalCount;
+
+    globalCount += 1;
+    cart.appendChild(div);
+}
+
+function search() {
+    const letter = document.getElementById("txt").value;
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${letter}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        itemList(data["drinks"]);
+    })
+    .catch(err => {
+        console.log("Error: ", err);
+        noItem();
+    });
+}
+
+function noItem() {
+    const item_list = document.getElementById("item-list");
+    item_list.innerHTML = "";
+
+    item_list.innerHTML = `
+        <font size="20px">No Items Found</font>
     `
 }
